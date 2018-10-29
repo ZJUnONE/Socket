d8f3600adde669f111a08b4afc6137a217ea765a
nONE <zjufenghang@gmail.com>
2018-10-29 22:25:59 +0800

    Update server.c

--- server.c
+++ server.c
@@ -1,58 +1,103 @@
-#include <sys/types.h>      
+#include <iostream>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
-#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/select.h>
+#define MAX_TIME 10		//Max waiting time
 
-#define SERVER_PORT 8000
-#define MAXLINE 4096
-int main(void)
+using namespace std;
+int main(int argc, char *argv[])
 {
-    struct sockaddr_in serveraddr, clientaddr;
-    int sockfd, addrlen, confd, len, i;
+    struct sockaddr_in s_in;//server address structure
+    struct sockaddr_in c_in;//client address structure
+    int l_fd,c_fd;
+    socklen_t len;
+    char buf[1024];//content buff area
+    char addr_p[16];
     char ipstr[128];
-    char buf[MAXLINE];
-
-    //1.socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    //2.bind
-    bzero(&serveraddr, sizeof(serveraddr));
-    /* 地址族协议IPv4 */
-    serveraddr.sin_family = AF_INET;
-    /* IP地址 */
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons(SERVER_PORT);
-    bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-    //3.listen
-    listen(sockfd, 128);
-    while (1) {
-        //4.accept阻塞监听客户端链接请求
-	printf("Waiting for accept.\n");
-        addrlen = sizeof(clientaddr);
-        confd = accept(sockfd, (struct sockaddr *)&clientaddr, &addrlen);//返回的是客户端和服务端专用通道的socket描述符
-        //输出客户端IP地址和端口号
-        inet_ntop(AF_INET, &clientaddr.sin_addr.s_addr, ipstr, sizeof(ipstr));
-        printf("Connection from client ip %s\tport %d\n", 
-                inet_ntop(AF_INET, &clientaddr.sin_addr.s_addr, ipstr, sizeof(ipstr)),
-                ntohs(clientaddr.sin_port));
-
-        //和客户端交互数据操作confd
-        //5.处理客户端请求 
-        len = read(confd, buf, sizeof(buf));
-	printf("Client message: %s\n",buf);
-        i = 0;
-        while (i < len) 
-	{
-            buf[i] = toupper(buf[i]);
-            i++;
-        }
-        write(confd, buf, len);
-
-        close(confd);
+    int port = 8000;
+	
+    int flag;
+	
+    int nfdp;
+    fd_set rfds;
+    fd_set wfds;
+    struct timeval time = {MAX_TIME, 0}; // 10s maximum delay.
+	
+    if (0)
+    {
+        printf("Usage with parameters [MasterKey] [port]\n");
+        exit(1);
     }
-    close(sockfd);
+    memset((void *)&s_in,0,sizeof(s_in));    
+    //bzero((void *)&s_in,sizeof(s_in));
+    s_in.sin_family = AF_INET;//IPV4 communication domain
+    s_in.sin_addr.s_addr = INADDR_ANY;//accept any address
+    s_in.sin_port = htons(port);//change port to netchar
+
+    l_fd = socket(AF_INET,SOCK_STREAM,0);//socket(int domain, int type, int protocol)
+
+    if( (bind(l_fd,(struct sockaddr *)&s_in,sizeof(s_in))) == -1)
+    {
+	cout << "Bind error!" << endl;
+	exit(-1);
+    }
+	
+    listen(l_fd,10);//lisiening start
+    while(1)
+	{
+		flag = 1;
+        cout<<"Waiting for connection."<<endl;
+
+        c_fd = accept(l_fd,(struct sockaddr *)&c_in,&len);
+        cout << "Client IP: " << inet_ntop(AF_INET, &c_in.sin_addr.s_addr, ipstr, sizeof(ipstr)) << ". Port: " << ntohs(c_in.sin_port) << endl;
+        while(flag)
+		{
+			FD_ZERO(&rfds);
+			FD_SET(c_fd,&rfds);
+			nfdp = c_fd + 1;
+			int j = select(nfdp, &rfds, NULL, NULL, &time);
+			switch(j)
+			{
+				case -1:
+					exit(-1);
+				case 0:
+				{
+					cout << "Connection closed because of 10s waiting!" << endl;
+					close(c_fd);
+					flag = 0;
+					break;
+				}
+				default:
+				{
+					int n = read(c_fd,buf,1024);//read the message send by client
+					if(n == 0)
+					{
+						cout << "Error!"<<endl;
+						exit(-1);
+					}
+					buf[n] = '\0';
+					if(n < 0)
+						cout << "Read error" << endl;
+					else if(!strcmp(buf, "exit"))
+					{
+						cout << "Connection closed" << endl;
+						close(c_fd);
+						flag = 0;
+					}
+					else
+					{
+						cout << "Client: "<< buf<<endl;
+						write(c_fd,buf,n);//sent message back to client
+					}
+				}
+			}
+		}
+    }
 
     return 0;
 }
